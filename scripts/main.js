47400471d0b97ccf5bbf7ee71abce1ecf21dd14d
tutinja <63499886+tutinja@users.noreply.github.com>
2020-04-11 16:15:53 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1303844793.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/1303844793.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+   if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = '45x Butterfly girl, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '45x Butterfly girl, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
